91e19e3d3093c5373dc86b8fc5b1ac44900308c1
Manish Tiwari <tmanish.in@gmail.com>
2025-12-02 04:32:57 -0600

    Revert "FullSync: Temporarily force full Sync to always succeed"
    This reverts commit e54f18c1bf88805e0036a4f0279f2266b6f88693.
    
    Change-Id: I3df3618269433fb2e47aa067ea58f85d72001254
    Signed-off-by: Manish Tiwari <tmanish.in@gmail.com>

--- src/manager.cpp
+++ src/manager.cpp
@@ -330,11 +330,9 @@ sdbusplus::async::task<bool>
         co_return co_await syncData(cfg, std::move(srcPath), retryCount);
     }
 
-    // NOTE: The following line is commented out as part of a temporary
-    // workaround. We are forcing Full Sync to succeed even if data syncing
-    // fails. This change should be reverted once proper error handling is
-    // implemented.
-    // setSyncEventsHealth(SyncEventsHealth::Critical);
+    // All retry attempts exhausted, mark sync event health as critical
+    setSyncEventsHealth(SyncEventsHealth::Critical);
+
     lg2::error("Sync failed after [{MAX_ATTEMPTS}] retries for [{SRC_PATH}]",
                "MAX_ATTEMPTS", cfg._retry->_retryAttempts, "SRC_PATH",
                currentSrcPath);
@@ -628,15 +626,8 @@ sdbusplus::async::task<void> Manager::startFullSync()
     }
     else
     {
-        // Forcefully marking full sync as successful, even if data syncing
-        // fails.
-        // TODO: Revert this workaround once the proper logic is implemented
-        setFullSyncStatus(FullSyncStatus::FullSyncCompleted);
-        setSyncEventsHealth(SyncEventsHealth::Ok);
-        lg2::info("Full Sync passed temporarily despite sync failures");
-
-        // setFullSyncStatus(FullSyncStatus::FullSyncFailed);
-        // lg2::info("Full Sync failed");
+        setFullSyncStatus(FullSyncStatus::FullSyncFailed);
+        lg2::info("Full Sync failed");
     }
 
     // total duration/time diff of the Full Sync operation
